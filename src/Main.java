fdd99d64458bec9257f875a2241826e0e6dbb55c
Star-Lord7 <samuelnasa375@gmail.com>
2024-10-31 21:47:30 -0600

    Cambios en rama Develop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Welcome Java Developer 18");
+        System.out.println("Github");
     }
 }
